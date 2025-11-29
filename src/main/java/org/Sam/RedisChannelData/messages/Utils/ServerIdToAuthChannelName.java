a42ac09eb4e21e8426183b4c4c4faa72085e7d6e
SaltyMini <samclaymichael@gmail.com>
2025-11-29 23:24:45 +0000

    Update ServerIdToAuthChannelName.java

--- src/main/java/org/Sam/RedisChannelData/messages/Utils/ServerIdToAuthChannelName.java
+++ src/main/java/org/Sam/RedisChannelData/messages/Utils/ServerIdToAuthChannelName.java
@@ -3,6 +3,10 @@ package org.Sam.RedisChannelData.messages.Utils;
 public class ServerIdToAuthChannelName {
 
     public static String serverIdToAuthChannelName(String serverID) {
-        return serverID + "_auth_channel";
+        return serverID + "_auth_update";
+    }
+
+    public static String serverIdToSubChannelName(String serverID) {
+        return serverID + "_sub_update";
     }
 }
